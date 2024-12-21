7bafc3998e48dc31fe075cfdde06f955e26979f6
Guddu kumar <guddukumaryadav10101@gmail.com>
2024-12-21 23:36:20 +0530

    try to check without routing

--- frontendapp/src/components/Header.jsx
+++ frontendapp/src/components/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import '../App.css'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div className='head'>
       
-       <ul>
+    <ul>
+      <li><a href='#'>Home</a></li>
+      <li><a href="#">About</a></li>
+    </ul>
+
+       {/* <ul>
         <li className='active' > <Link to="/">Home</Link> </li>
         
         <li><Link to={"/about"}>About-Us</Link> </li>
         <li><Link to={"/contact/"}>Contact-Us</Link> </li>
         <li><Link to={"/enquiry"}>Enquiry</Link> </li>
         <li><Link to={"/signup/"}>Signup/Login</Link></li>
-         </ul>
+
+         </ul> */}
       
     </div>
   )
--- frontendapp/src/main.jsx
+++ frontendapp/src/main.jsx
@@ -1,33 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Signup from './components/Signup'
 import Header from './components/Header'
 import About from './components/About'
 import Contact from './components/Contact-Us'
 import Enquiry from './components/Enquiry'
-let routers=createBrowserRouter([
-  {
-    path:'/',element:<Header/>
-  },
-  {
-    path:'signup/',element:<Signup/>
-  },
-  {
-    path:'about/',element:<About/>
-  },
-  {
-    path:'contact/',element:<Contact/>
-  }
-  ,{
-    path:'enquiry/',element:<Enquiry/>
-  }
+// let routers=createBrowserRouter([
+//   {
+//     path:'/',element:<Header/>
+//   },
+//   {
+//     path:'signup/',element:<Signup/>
+//   },
+//   {
+//     path:'about/',element:<About/>
+//   },
+//   {
+//     path:'contact/',element:<Contact/>
+//   }
+//   ,{
+//     path:'enquiry/',element:<Enquiry/>
+//   }
 
-])
+// ])
 createRoot(document.getElementById('root')).render(
 
 
   <StrictMode>
-  <RouterProvider router={routers} />
+  {/* <RouterProvider router={routers} /> */}
+  <Header/>
   </StrictMode>,
 )
